d32f883c9a3ce7418d49aa0f94e2821a500ce9a3
Matt Conway <matt@cloudtruth.com>
2022-01-31 20:14:52 +0000

    cloudtruth-cli: add 1.1.6 bottle.

--- Formula/cloudtruth-cli.rb
+++ Formula/cloudtruth-cli.rb
@@ -5,6 +5,12 @@ class CloudtruthCli < Formula
   sha256 "97ddc0107c1d234a00f4921fccbce42360ec1e1527c12b21ccdc9fad38e6bd86"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/cloudtruth/homebrew-formulae/releases/download/cloudtruth-cli-1.1.6"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur: "d8a3aa7fa4a42c729e81aeb5698fc7745c9236514ec686f5b75b6d1ee866a2fa"
+  end
+
   depends_on "rust" => :build
 
   def install
